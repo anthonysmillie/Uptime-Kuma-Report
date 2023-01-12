3ffa9ca4cce9793c3a802379f22d6c0349edfc5c
mehdipyw <mehdiasadpourpyw@gmail.com>
2023-01-12 13:39:18 +0330

    interval-included

--- kumareport/database.py
+++ kumareport/database.py
@@ -1,6 +1,9 @@
 import sqlite3
 
 from datetime import datetime
+from tokenize import Double
+from urllib import request
+from winreg import DeleteValue
 
 
 class Database:
@@ -8,7 +11,6 @@ class Database:
 
     def __init__(self, database):
         Database.db = self
-
         self.conn = self.create_connection(database)
 
     def cursor(self):
@@ -28,17 +30,29 @@ class Database:
 
         return conn
 
+    def get_monitor_interval(self, monitor_id):
+        cur = self.conn.cursor()
+        cur.execute("SELECT interval FROM monitor WHERE id=?", (monitor_id,))
+        result = cur.fetchone()
+
+        return result
+
+    def number_of_requests(self, start: datetime, end: datetime):
+        delta = (end - start)*24*60*60
+
+        return delta.days
+
     def count_heartbeat_by_status(
             self, monitor_id, status, start: datetime, end: datetime
     ):
         cur = self.conn.cursor()
-        cur.execute("SELECT count(*) FROM heartbeat WHERE monitor_id=? AND status=? AND time>? AND time <?",
+        cur.execute("SELECT count(*) FROM heartbeat WHERE monitor_id=? AND status=? AND time>=? AND time<?",
                     (monitor_id, status, start, end))
         result = cur.fetchone()
 
         return result[0]
 
-    def percent_by_monitor_id(self, monitor_id, start: datetime, end: datetime):
+    def percent_by_monitor_id_1(self, monitor_id, start: datetime, end: datetime):
         rows = self.count_heartbeat_by_monitor_id(monitor_id, start, end)
         result = self.count_heartbeat_by_status(monitor_id, 1, start, end)
 
@@ -46,6 +60,17 @@ class Database:
             return 0
 
         percentage = (result / rows) * 100
+        return percentage
+
+    def percent_by_monitor_id_2(self, monitor_id, start: datetime, end: datetime):
+        rows = self.get_monitor_interval(monitor_id,)
+        result = self.count_heartbeat_by_status(monitor_id, 1, start, end)
+        
+        if rows == 0:
+            return 0
+
+        percentage = (
+            result / (self.number_of_requests(start , end) / int(rows[0]))) * 100
         return percentage
 
     def count_heartbeat_by_monitor_id(
